9ca101383b7b622fb04b814607921d09876c0273
andytlai <andylai3@gmail.com>
2024-06-01 22:43:21 +0000

    add summary from files

--- sentiment.py
+++ sentiment.py
@@ -4,11 +4,12 @@ from langchain_openai import ChatOpenAI
 from langchain_community.vectorstores import AstraDB
 from langchain.schema.runnable import RunnableMap
 from langchain.prompts import ChatPromptTemplate
+import csv_reader as cr
 
 # Cache prompt for future runs
 @st.cache_data()
 def load_prompt():
-    template = """You're a helpful AI Trading Agent that is given a list of tweets about stocks. Classify each tweet as bullish, bearish or neutral.   If it's not clear what the sentiment of the tweet is, return unknown.   Additionally, please summarize the tweets and give an overall recommendation of bullish, bearish or neutral for the given stock.
+    template = """You're a helpful AI Trading Agent that gets a list of the 10 latest tweets. Classify each tweet as bullish, bearish or neutral.   If it's not clear what the sentiment of the tweet is, return unknown.   Additionally, please summarize the tweets and give an overall recommendation of bullish, bearish or neutral for the given stock.
  
 Tweets:
 {tweets}
@@ -33,15 +34,21 @@ if 'messages' not in st.session_state:
     st.session_state.messages = []
 
 # Draw a title and some markdown
-st.title("Your personal AI Trading Agent Sentiment Analysis")
-st.markdown("""Enter your list of tweets to get sentiment analysis!""")
+# Draw a title and some markdown
+st.title("~~~Trading Agent~~~")
+st.markdown("""Trading Agent""")
+
+# Get the first column values from 10 random rows
+first_column_values = cr.read_random_rows_from_csv()
+st.markdown("Tweets:")
+st.markdown(first_column_values)
 
 # Draw all messages, both user and bot so far (every time the app reruns)
 for message in st.session_state.messages:
     st.chat_message(message['role']).markdown(message['content'])
 
 # Draw the chat input box
-if tweets := st.chat_input("Send me tweets?"):
+if tweets := st.chat_input("Get me tweets?"):
     
     # Store the user's tweets in a session object for redrawing next time
     st.session_state.messages.append({"role": "human", "content": tweets})
@@ -55,7 +62,7 @@ if tweets := st.chat_input("Send me tweets?"):
         'tweets': lambda x: x['tweets']
     })
     chain = inputs | prompt | chat_model
-    response = chain.invoke({'tweets': tweets})
+    response = chain.invoke({'tweets': first_column_values})
     answer = response.content
 
     # Store the bot's answer in a session object for redrawing next time
